c12c455cbad992658532d01aec3b2a72f9b44056
Thomas L. <thomas@twobrains.com>
2026-08-14 22:33:47 +0200

    feat(previous_mistakes): allowing selection of mistakes to create study set with just the selected mistakes for additional practising

--- apps/front/src/app/card-mistakes/card-mistakes.module.ts
+++ apps/front/src/app/card-mistakes/card-mistakes.module.ts
@@ -3,12 +3,16 @@ import { CardMistakesComponent } from "./card-mistakes.component";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { CardMistakesRoutingModule } from "./card-mistakes-routing.module";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
     CardMistakesRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    SharedModule
   ],
   declarations: [CardMistakesComponent]
 })
