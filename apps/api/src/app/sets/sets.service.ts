d05964ff95cb2e2f55b377169c5681fb5cb716b8
Thomas L. <thomas@twobrains.com>
2026-08-24 13:14:40 +0200

    chore(cleanup): consolidated both jwt payload decodes to single 'jsonwebtoken' package instead of using two packages

--- apps/api/src/app/sets/sets.service.ts
+++ apps/api/src/app/sets/sets.service.ts
@@ -3,7 +3,7 @@ import { PrismaService } from "../providers/database/prisma/prisma.service";
 import { Prisma } from "@scholarsome/prisma";
 import { Set } from "@scholarsome/shared";
 import { Request as ExpressRequest } from "express";
-import { jwtDecode } from "jwt-decode";
+import * as jwt from "jsonwebtoken";
 import { UsersService } from "../users/users.service";
 import { StorageService } from "../providers/storage/storage.service";
 
@@ -36,7 +36,7 @@ export class SetsService {
     let accessToken: { id: string; email: string; };
 
     if (req.cookies && req.cookies["access_token"]) {
-      accessToken = jwtDecode(req.cookies["access_token"]) as { id: string; email: string; };
+      accessToken = jwt.decode(req.cookies["access_token"]) as { id: string; email: string; };
     } else {
       return false;
     }
